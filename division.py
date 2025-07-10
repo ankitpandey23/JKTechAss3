915d505e6ad94ed02e3d3223ee59349c6340fff8
ankitpandey23 <ankit.pandey@jktech.com>
2025-07-10 11:39:08 +0530

    Update division.py

--- division.py
+++ division.py
@@ -1,7 +1,4 @@
 def divide(numbers):
-    """
-    return the division of first/last numbers in a list
-    """
     if numbers[-1] == 0:
         raise ValueError("Cannot divide by zero.")
     return round(numbers[0] / numbers[-1])
